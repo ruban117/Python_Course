47295c8092be0280a7c0ed69c57dd3d0eb3d872a
Ruban Pathak <102974324+ruban117@users.noreply.github.com>
2022-12-17 13:39:02 +0530

    Update Rollar_Costar_Ride.py

--- Rollar_Costar_Ride.py
+++ Rollar_Costar_Ride.py
@@ -1,21 +1,27 @@
 print("Welcome to the rollercoaster!")
 height = int(input("What is your height in cm? "))
 bill = 0
+
 if height >= 120:
-    print("You can ride into rollarcoster.")
+    print("You can ride into the rollarcostar")
     age = int(input("Enter your age: "))
     if age <= 12:
-        bill = 5
-        print("Please Pay $5")
+        bill += 5
+        print("You have to pay $5")
     elif age <= 18:
-        bill = 7
-        print("Please pay $7")
-    elif age > 18:
-        bill = 12
-        print("Please pay $12")
-        want_photo = input("Do you want some photos?: 'Y' or 'N' ")
-        if want_photo == 'Y':
+        bill += 7
+        print("You have to pay $7")
+    elif age >= 45 and age <= 55:
+        bill += 0
+        print("You do not have to pay.")
+    else:
+        bill += 12
+        print("You have to pay $12")
+        want_pic = input("Do you want pictures? 'Y' or 'N' ")
+        if want_pic == 'Y':
             bill += 3
-            print(f"So your total bill is {bill}")
-else:
-    print("Sorry, you can't ride into the rollarcoster!")
+            print(f"Your final bill is ${bill}")
+        else:
+            print(f"Your final bill is ${bill}")
+else :
+  print("Sorry, you can't ride into the rollarcoster!")
