4a03eeb67e4707ae1342a8a0e19499f303f11417
Ruban Pathak <102974324+ruban117@users.noreply.github.com>
2022-12-18 11:09:22 +0530

    Add files via upload

---
+++ Heads_Tails.py
@@ -0,0 +1,13 @@
+#Remember to use the random module
+#Hint: Remember to import the random module here at the top of the file. 🎲
+	 
+#Write the rest of your code below this line 👇
+
+import random
+
+prob=random.randint(0 , 1)
+
+if prob == 1 :
+    print("Heads")
+else :
+    print("Tails")
---
+++ Pyrhon_Lists.py
@@ -0,0 +1,13 @@
+states_of_america = ["Delaware", "Pennsylvania", "New Jersey", "Georgia", "Connecticut", "Massachusetts", "Maryland", "South Carolina", "New Hampshire", "Virginia", "New York", "North Carolina", "Rhode Island", "Vermont", "Kentucky", "Tennessee", "Ohio", "Louisiana", "Indiana", "Mississippi", "Illinois", "Alabama", "Maine", "Missouri", "Arkansas", "Michigan", "Florida", "Texas", "Iowa", "Wisconsin", "California", "Minnesota", "Oregon", "Kansas", "West Virginia", "Nevada", "Nebraska", "Colorado", "North Dakota", "South Dakota", "Montana", "Washington", "Idaho", "Wyoming", "Utah", "Oklahoma", "New Mexico", "Arizona", "Alaska", "Hawaii"]
+
+states_of_america.append("Mia Khalifa Land")
+states_of_america.extend(["Ava Addams Land", "Sunny Leone Land"])
+states_of_america[5]="MKC"
+
+print(states_of_america[-9])
+print(states_of_america[0])
+print(states_of_america)
+
+
+dirty_dozen = ["Strawberries", "Spinach", "Kale", "Nectarines", "Apples", "Grapes", "Peaches", "Cherries", "Pears", "Tomatoes", "Celery", "Potatoes"]
+
