a0ae23eddcdda71dbf4f67f059d5e932a6449198
Ruban Pathak <102974324+ruban117@users.noreply.github.com>
2022-12-20 18:58:32 +0530

    Add files via upload

---
+++ Robot_Square_Function.py.py
@@ -0,0 +1,19 @@
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+
+#drawing square
+turn_left()
+move()
+turn_right()
+move()
+turn_right()
+move()
+turn_right()
+move()
+
+################################################################
+# WARNING: Do not change this comment.
+# Library Code is below.
+################################################################
