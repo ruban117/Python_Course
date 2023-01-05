3cbcdcae00a3174dcb6972f526c6b8fa9bd6400d
Ruban Pathak <102974324+ruban117@users.noreply.github.com>
2023-01-05 13:27:52 +0530

    Add files via upload

---
+++ Caeser_Cipher.py
@@ -0,0 +1,66 @@
+import art
+print(art.logo)
+logo = """           
+ ,adPPYba, ,adPPYYba,  ,adPPYba, ,adPPYba, ,adPPYYba, 8b,dPPYba,  
+a8"     "" ""     `Y8 a8P_____88 I8[    "" ""     `Y8 88P'   "Y8  
+8b         ,adPPPPP88 8PP"""""""  `"Y8ba,  ,adPPPPP88 88          
+"8a,   ,aa 88,    ,88 "8b,   ,aa aa    ]8I 88,    ,88 88          
+ `"Ybbd8"' `"8bbdP"Y8  `"Ybbd8"' `"YbbdP"' `"8bbdP"Y8 88   
+            88             88                                 
+           ""             88                                 
+                          88                                 
+ ,adPPYba, 88 8b,dPPYba,  88,dPPYba,   ,adPPYba, 8b,dPPYba,  
+a8"     "" 88 88P'    "8a 88P'    "8a a8P_____88 88P'   "Y8  
+8b         88 88       d8 88       88 8PP""""""" 88          
+"8a,   ,aa 88 88b,   ,a8" 88       88 "8b,   ,aa 88          
+ `"Ybbd8"' 88 88`YbbdP"'  88       88  `"Ybbd8"' 88          
+              88                                             
+              88           
+"""
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+def caesar(plain_text, shift_amount, p_direction):
+    cipher_text = ""
+    if p_direction == "encode":
+        for letter in plain_text:
+          if letter in alphabet:
+            position = alphabet.index(letter)
+            new_position = position + shift_amount
+            cipher_text += alphabet[new_position]
+          else:
+            cipher_text+=letter
+        print(f"The encoded text is {cipher_text}")
+    elif p_direction == "decode":
+        for letter in plain_text:
+          if letter in alphabet:
+            position = alphabet.index(letter)
+            new_position = position - shift_amount
+            cipher_text += alphabet[new_position]
+          else:
+            cipher_text+=letter
+        print(f"The decoded text is {cipher_text}")
+
+#TODO-1: Import and print the logo from art.py when the program starts.
+
+#TODO-4: Can you figure out a way to ask the user if they want to restart the cipher program?
+#e.g. Type 'yes' if you want to go again. Otherwise type 'no'.
+#If they type 'yes' then ask them for the direction/text/shift again and call the caesar() function again?
+#Hint: Try creating a while loop that continues to execute the program if the user types 'yes'. 
+end=True
+while end:
+  direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+  text = input("Type your message:\n").lower()
+  shift = int(input("Type the shift number:\n"))
+  shift=shift%26
+  #TODO-2: What if the user enters a shift that is greater than the number of letters in the alphabet?
+  #Try running the program and entering a shift number of 45.
+  #Add some code so that the program continues to work even if the user enters a shift number greater than 26. 
+  #Hint: Think about how you can use the modulus (%).
+  
+  caesar(plain_text=text, shift_amount=shift, p_direction=direction)
+  con=input("Type yes to continue or no to stop:\n")
+  if con == "yes":
+    end=True
+  elif con == "no":
+    print("Goodbye!")
+    end=False
